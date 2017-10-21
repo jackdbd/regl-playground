0084944ac8f30e1a7f7feaaf869423f75ee09025
jackdbd <jackdebidda@gmail.com>
2017-10-21 18:04:05 +0200

    Animate dots

--- src/js/dots-2d.js
+++ src/js/dots-2d.js
@@ -31,11 +31,21 @@ const drawDots = regl({
 
   attributes: {
     xyPosition: (context, props) => {
+      // context.time is the elapsed seconds since the start of the app
+      // context.tick is the elapsed number of frames since the start of the app
+      const x1 = props.sineWaveFunction({ t: context.time });
+      const x2 = props.sineWaveFunction({ t: context.time, amplitude: 0.5 });
+      const x3 = props.sineWaveFunction({ t: context.time, tau: 2 });
+      const y4 = props.sineWaveFunction({ t: context.time, tau: 5 });
+      const pos = props.sineWaveFunction({ t: context.time, tau: 2 });
+
       // this is a 2D visualization, so each vertex has 2 coordinates
       const vertices = [
-        [0.9, 0.9],
-        [0.0, 0.0],
-        [-0.7, -0.7],
+        [x1, 0.9],
+        [x2, 0],
+        [x3, -0.7],
+        [0.8, y4],
+        [pos, pos],
       ];
       return vertices;
     },
@@ -49,7 +59,7 @@ const drawDots = regl({
   },
 
   primitive: 'points',
-  count: 3,
+  count: 5,
 });
 
 // https://github.com/regl-project/regl/blob/gh-pages/API.md#context
@@ -66,5 +76,19 @@ regl.frame((context) => {
   drawDots({
     color: [0.208, 0.304, 1.000, 1.000],
     pointWidth: 15.0,
+    /**
+     * Sinusoidal function.
+     * @param {Number} t         Time (in seconds).
+     * @param {Number} tau       Period of the sine wave (in seconds).
+     * @param {Number} amplitude Amplitude of the sine wave.     
+     * @param {Number} fi        Phase of the sine wave (in radians).
+     * @return {Number} pos      Position
+     */
+    sineWaveFunction: ({ t, tau = 1, amplitude = 1, fi = 0 }) => {
+      // angular frequency (in radians per second)
+      const omega = (2 * Math.PI) / tau;
+      const pos = amplitude * Math.sin((omega * t) + fi);
+      return pos;
+    },
   });
 });
